fac97cd771d67d98f700a02aff1530c52cb0ae00
Caryl Tan <tan.caryl@gmail.com>
2023-03-18 09:57:31 -0400

    ternary for vote or no vote

--- src/Pages/VotingBooth.js
+++ src/Pages/VotingBooth.js
@@ -34,50 +34,32 @@ const VotingBooth = () => {
   // function to record vote submitted on button submit
   function handleSubmitVote(e) {
     e.preventDefault();
-    // setIsSubmitted(true);
+
 
     const queryParameters = window.location.href.split('/');
-    const lastSegment = queryParameters.pop()
+    const lastSegment = queryParameters.pop();
     const dbRef = ref(auth, lastSegment);
-   
+
     get(dbRef).then((snapshot) => {
       const fullPollData = snapshot.val().pollOptions;
       fullPollData.map((pollOption, index) => {
-          if (getValue === pollOption.pollOption){
-            pollOption.votes = pollOption.votes + 1
-            update(dbRef, {
-              [`pollOptions/${index}/votes`]: pollOption.votes
+        if (getValue === pollOption.pollOption) {
+          pollOption.votes = pollOption.votes + 1
+          update(dbRef, {
+            [`pollOptions/${index}/votes`]: pollOption.votes
+          });
+          setIsSubmitted(true);
+        }  else if (getValue == null){
+            Swal.fire({
+              icon: "warning",
+              title: "Oops...",
+              text: "You must select a vote...or else!"
             });
+            setIsSubmitted(false);
+            return;
           }
-        })
+      })
     })
-    
-
-    
-
-    // }
-    // if (getValue === "pollOptionOne") {
-    //   votingObject.pollOptionOne.votes = votingObject.pollOptionOne.votes + 1;
-    //   votingObject.totalVotes = votingObject.pollOptionOne.votes + votingObject.pollOptionTwo.votes;
-    // } else if (getValue === "pollOptionTwo") {
-    //   votingObject.pollOptionTwo.votes = votingObject.pollOptionTwo.votes + 1;
-    //   votingObject.totalVotes = votingObject.pollOptionOne.votes + votingObject.pollOptionTwo.votes;
-    // } else {
-    // alert for user if no vote was submitted
-    //   Swal.fire({
-    //     icon: "warning",
-    //     title: "Oops...",
-    //     text: "You must choose an option, fence-sitter!"
-    //   });
-    //   //if error returns, isSubmitted state is false to prevent vote confirmation ternary
-    //   setIsSubmitted(false);
-    //   return;
-    // }
-
-    // reference database
-   
-    //update vote to firebase
-    
   };
 
   //function to discern poll option value choices on event
@@ -88,8 +70,9 @@ const VotingBooth = () => {
   // function to handle copy link to clipboard
   const clickHandler = (e, poll) => {
     e.preventDefault();
-    //copy url of voting booth
-    navigator.clipboard.writeText(`whatever-floats-your-vote.netlify.app/votingbooth/${poll.key}`)
+    const queryParameters = window.location.href.split('/');
+    const lastSegment = queryParameters.pop()
+    navigator.clipboard.writeText(`whatever-floats-your-vote.netlify.app/votingbooth/${lastSegment}`)
     //alert when link is copied to clipboard
     Swal.fire({
       icon: "success",
@@ -97,58 +80,49 @@ const VotingBooth = () => {
       showConfirmButton: false,
       timer: 1500
     });
-    //isSubmitted set to false to prevent ternary for vote confirmation component
     setIsSubmitted(false);
     return;
   }
 
   return (
     <>
-      <div className="voting-question">
-        <h2>Question <span className="poll-heading">{pollData && pollData.pollQuestion}</span></h2>
-      </div>
-
-      <form onSubmit={(e) => { handleSubmitVote(e) }}>
-
-        <div className="selection-container">
-          <fieldset onChange={onChangeValue}>
-            {pollData.pollOptions &&
-              pollData.pollOptions.map((poll) => {
-                return (
-                  <div>
-                    <input type="radio" name="choice" value={poll.pollOption} />
-                    <label htmlFor={poll.pollOption}>{poll.pollOption}</label>
-                  </div>
-                )
-              })
-            }
-          </fieldset>
-        </div>
-
-
-        <div className="button-container">
-          <button className="button primary" type="submit">Submit</button>
-          <div className="secondary-buttons">
-            <button className="button secondary" aria-label="Copy poll link to keyboard." value="Copy" onClick={(e) => { clickHandler(e) }}>Copy Poll Link</button>
-            <Link className="button secondary" to={`/results/${boothID}`}>See Results Only</Link>
+      {isSubmitted ?
+        <VotingConfirmation /> :
+        <>
+          <div className="voting-question">
+            <h2>Question <span className="poll-heading">{pollData && pollData.pollQuestion}</span></h2>
           </div>
-        </div>
 
-      </form>
+          <form onSubmit={(e) => { handleSubmitVote(e) }}>
+
+            <div className="selection-container">
+              <fieldset onChange={onChangeValue}>
+                {pollData.pollOptions &&
+                  pollData.pollOptions.map((poll) => {
+                    return (
+                      <div>
+                        <input type="radio" name="choice" value={poll.pollOption} />
+                        <label htmlFor={poll.pollOption}>{poll.pollOption}</label>
+                      </div>
+                    )
+                  })
+                }
+              </fieldset>
+            </div>
+
+
+            <div className="button-container">
+              <button className="button primary" type="submit">Submit</button>
+              <div className="secondary-buttons">
+                <button className="button secondary" aria-label="Copy poll link to keyboard." value="Copy" onClick={(e) => { clickHandler(e) }}>Copy Poll Link</button>
+                <Link className="button secondary" to={`/results/${boothID}`}>See Results Only</Link>
+              </div>
+            </div>
+          </form>
+        </>
+      }
     </>
   )
 }
-
-
-//   return (
-//     <>
-//       {isSubmitted ?
-//         <VotingConfirmation boothID={boothID} /> :
-//         <section className="voting-ticket">
-//         </section>
-//       }
-//     </>
-//   );
-// };
 
 export default VotingBooth;
