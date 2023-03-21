3561e3084baf21bae461b40fa859b4faa72068b6
Caryl Tan <tan.caryl@gmail.com>
2023-03-20 20:36:45 -0400

    error page styled

--- src/Pages/ErrorPage.js
+++ src/Pages/ErrorPage.js
@@ -1,15 +1,15 @@
 //Modules
 import { Link } from "react-router-dom";
 //Assets
-import ErrorGraphic from "../assets/error-page-graphic.svg";
+import ErrorGraphic from "../assets/404-error.png";
 
 const ErrorPage = () => {
   return (
     <>
-      <div className="error-img">
-      <img src={ErrorGraphic} alt="Illustration of person holding a hammer in front of a computer screen with a bug on it."/>
+      <div className="error-container">
+        <img src={ErrorGraphic} alt="404 error image. Astronaut lost in space!"/>
       </div>
-      <h2>Oh no! Page not found!</h2>
+      <h2 className="error-h2">Oh no! Page not found!</h2>
       <div className="error-btn">
         <Link to={`/`} className="button primary"> Home</Link>
       </div>
